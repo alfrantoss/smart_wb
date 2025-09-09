2aa3a9209664e9d2ffccf99dc0e319a305bd2ab4
Azril Ardiansyah <ardiansyahazril9@gmail.com>
2025-09-09 16:26:38 +0700

    gw ngerubah indexphp

--- mypras/index.php
+++ mypras/index.php
@@ -13,7 +13,7 @@
                     </div>
                     <div class="hidden-on-mobile">
                      <?php if($user['level']=='admin'){ ?> <?= $user['nama'] ?> <?php } ?>
-					  <p style="color:green">online</p>
+					  <p style="color:green">onlineeeeeeeeeeeee</p>
                     </div>
                     <div class=" hidden-on-mobile">         
                       <?= $setting['sekolah'] ?>
